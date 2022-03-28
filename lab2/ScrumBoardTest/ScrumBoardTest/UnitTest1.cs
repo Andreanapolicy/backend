7bc56e6a8401b629c5de04d6fe83f702b77f7a5e
Andrey Dudin <andrewdudin1337@gmail.com>
2022-03-28 16:55:43 +0300

    ScrumBoardTests. Добавил инициализацию фабрик в тестах

--- lab2/ScrumBoardTest/ScrumBoardTest/UnitTest1.cs
+++ lab2/ScrumBoardTest/ScrumBoardTest/UnitTest1.cs
@@ -1,14 +1,28 @@
 using NUnit.Framework;
 using ScrumBoard.ScrumBoard.Board;
+using ScrumBoard.ScrumBoard.Board.BoardFactory;
+using ScrumBoard.ScrumBoard.Column.ColumnFactory;
+using ScrumBoard.ScrumBoard.Task.TaskFactory;
 
 namespace ScrumBoardTest;
 
 public class Tests
 {
+    private IBoardFactory boardFactory;
+    private IColumnFactory columnFactory;
+    private ITaskFactory taskFactory;
+
+    public Tests()
+    {
+        this.boardFactory = new BoardFactory();
+        this.columnFactory = new ColumnFactory();
+        this.taskFactory = new TaskFactory();
+    }
+
     [Test]
     public void Test1()
     {
-        IBoard board = new Board("Site Services(SS)");
+        IBoard board = this.boardFactory.createBoard("Site Services(SS)");
         Assert.AreEqual(board.Name, "Site Services(SS)");
     }
 }
